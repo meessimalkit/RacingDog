12d0862070da5c10b9244dbd66d936b8fa3f2b5e
Malkit <81338544+meessimalkit@users.noreply.github.com>
2021-06-02 11:19:40 +1200

    Malkit

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using dograce;
+
+namespace UnitTestProject //unit testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void checkfourdogs()
+        {
+              Greyhound[] GreyHound = new Greyhound[4];
+       
+    }
+
+        [TestMethod]
+        public void checkthreeplayers()
+        {
+              Punter[] Punter = new Punter[3];
+
+    }
+    }
+}
